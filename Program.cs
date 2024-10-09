5b4ae1e7f0bb2d5491364c561fa42f3e4bd2dd5b
Luna <lunaleitora@gmail.com>
2024-10-09 18:36:46 +0100

    comit initial

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int age = 9;
+
+//string result = $"a Luna tem{age}";
+
+//Console.WriteLine(result);
+
+if ( age<=30){
+    Console.WriteLine("jovem");
+}else{
+    Console.WriteLine("velho");
+}

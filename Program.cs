6594de4c83d7ccecacfc11724d6888edef7ab42e
Luna <lunaleitora@gmail.com>
2024-10-10 10:47:17 +0100

    tarefa2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,29 @@
-﻿int age = 9;
+﻿int toys ;
+
+int age = 9;
+
+string name ;
+
+Console.WriteLine("digite seu nome");
+
+name = Console.ReadLine();
+
+Console.WriteLine("digite sua quantidade de brinquedos que seje numeros");
+
+toys = int.Parse( Console.ReadLine() );
 
 //string result = $"a Luna tem{age}";
 
 //Console.WriteLine(result);
 
-if ( age<=30){
-    Console.WriteLine("jovem");
-}else{
-    Console.WriteLine("velho");
+if ( toys>=20){
+    Console.WriteLine($"papabéns {name} vc é RICO");
+}
+
+else if ( toys>=15){
+Console.WriteLine($"vc é bem pago");
+}
+
+else{
+    Console.WriteLine($" vc é muito pobre {name} vc só tem {toys} brinquedos kkkkkkkkkkk");
 }
